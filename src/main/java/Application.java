f68c9a265d9e64847a7669d0237428272b747e9e
matko <martin.kranec@gmail.com>
2018-09-24 22:07:11 +0200

    End of section commit

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,12 @@
 import com.pluralsight.service.CustomerService;
 import com.pluralsight.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String[] args){
-        CustomerService service =new CustomerServiceImpl();
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CustomerService service =applicationContext.getBean("customerService",CustomerService.class);
         System.out.println(service.findAll().get(0).getFirstname()+":"+service.findAll().get(0).getLastName());
         System.out.println(service.findAll().get(1).getFirstname()+":"+service.findAll().get(1).getLastName());
     }
--- src/main/java/com/pluralsight/service/CustomerServiceImpl.java
+++ src/main/java/com/pluralsight/service/CustomerServiceImpl.java
@@ -1,17 +1,30 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
-import com.pluralsight.repository.HibernateCustomerRepositoryImpl;
 import com.pluralsight.repository.ICustomerRepository;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
-    private ICustomerRepository customerRepository =new HibernateCustomerRepositoryImpl();
+    private ICustomerRepository customerRepository;
+
+    public CustomerServiceImpl() {
+    }
+
+    public CustomerServiceImpl(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public List<Customer> findAll(){
         return  customerRepository.findAll();
     }
 
+    public ICustomerRepository getCustomerRepository() {
+        return customerRepository;
+    }
+
+    public void setCustomerRepository(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 }
